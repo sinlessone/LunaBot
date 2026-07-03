1df74fc24a2135666cfe5517ff2d90e46700e3bf
moh <mohthegoat1@proton.me>
2026-07-03 14:07:04 +0100

    fix: update permission check to use PermissionsBitField in handlehoneypot

--- handlers/handlehoneypot.js
+++ handlers/handlehoneypot.js
@@ -1,9 +1,10 @@
 const {queryone, db} = require("../utils/db");
+const {PermissionsBitField} = require("discord.js");
 
 
 module.exports = async function (message, client) {
     if (message.author.bot) return;
-    if (message.member.permissions.has("ADMINISTRATOR") || message.guild.ownerId) return
+    if (message.member.permissions.has(PermissionsBitField.Flags.Administrator) || message.guild.ownerId) return
     try {
         await message.member.ban({
             reason: "compromised account"
