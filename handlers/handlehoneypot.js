aa5444ca7f8da7593e166b3f5d6481e418c3df20
moh <mohthegoat1@proton.me>
2026-07-03 14:15:08 +0100

    fix: correct owner ID check for bypassing ban restrictions in handlehoneypot

--- handlers/handlehoneypot.js
+++ handlers/handlehoneypot.js
@@ -4,15 +4,17 @@ const {PermissionsBitField} = require("discord.js");
 
 module.exports = async function (message, client) {
     if (message.author.bot) return;
-    if (message.member.permissions.has(PermissionsBitField.Flags.Administrator) || message.guild.ownerId) return
+    if (message.member.permissions.has(PermissionsBitField.Flags.Administrator) || message.guild.ownerId === message.author.id) return
     try {
+        console.log("im here")
         await message.member.ban({
-            reason: "compromised account"
+            reason: "compromised account",
+            deleteMessageSeconds: 7 * 24 * 60 * 60
         })
         await message.delete()
         const { softbanorban } = await queryone(db, "SELECT softbanorban FROM serverconfig WHERE server_id=$1", [message.guild.id])
         if (!softbanorban) {
-            await message.member.unban()
+            await message.guild.bans.remove(message.author.id)
         }
     } catch (e) {
         console.error(e)
