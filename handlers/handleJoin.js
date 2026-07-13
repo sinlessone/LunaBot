6d02b981e5430dc28d93b1a2a3a9eca079e89040
moh <mohthegoat1@proton.me>
2026-07-13 17:55:21 +0100

    feat: enhance handleJoin to conditionally add roles and send welcome messages based on configuration

--- handlers/handleJoin.js
+++ handlers/handleJoin.js
@@ -4,15 +4,20 @@ exports.handleJoin = async (client, member) => {
     console.log(member);
     try {
         const {joinchannelid, joinroleid} = await queryone(db, "SELECT * FROM serverconfig WHERE server_id=$1", [member.guild.id]);
-        const channel = await client.channels.fetch(joinchannelid);
-        console.log(member.user.displayAvatarURL());
-        await member.roles.add(joinroleid)
-        await channel.send({
-            embeds: [presets.success("", `hope you have a great stay here <@${member.user.id}>!\n We now have ${member.guild.memberCount} members!`).setAuthor({
-                name: "Welcome to the server!",
-                iconURL: member.user.displayAvatarURL(),
-            })],
-        })
+
+        if (joinroleid) {
+            await member.roles.add(joinroleid)
+        }
+        if (joinchannelid) {
+            const channel = await client.channels.fetch(joinchannelid);
+            await channel.send({
+                embeds: [presets.success("", `hope you have a great stay here <@${member.user.id}>!\n We now have ${member.guild.memberCount} members!`).setAuthor({
+                    name: "Welcome to the server!",
+                    iconURL: member.user.displayAvatarURL(),
+                })],
+            })
+        }
+
     } catch (e) {
         console.log(e)
     }
