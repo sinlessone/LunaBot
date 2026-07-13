04dd75fdf8d745bb5c169cd0f73bbcc7de30e9ce
moh <mohthegoat1@proton.me>
2026-07-13 17:56:24 +0100

    feat: remove console log from handleJoin for cleaner output

--- handlers/handleJoin.js
+++ handlers/handleJoin.js
@@ -1,7 +1,6 @@
 const {queryone, db} = require("../utils/db");
 const {presets} = require("../data/embed");
 exports.handleJoin = async (client, member) => {
-    console.log(member);
     try {
         const {joinchannelid, joinroleid} = await queryone(db, "SELECT * FROM serverconfig WHERE server_id=$1", [member.guild.id]);
 
