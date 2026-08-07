32deea4f3b8c43c4b0aa3ca6c2497f1c96754b16
moh <mohthegoat1@proton.me>
2026-08-07 15:01:33 +0100

    feat: add error handling to handlecounting function

--- handlers/handlecounting.js
+++ handlers/handlecounting.js
@@ -3,51 +3,56 @@ const {presets} = require("../data/embed");
 const parseAnyNumber = require("../utils/toArabicNum");
 module.exports = {
     async handlecounting(message) {
-        let msg = message.content.toString().toLowerCase().split(" ")
-        msg[0] = msg[0].replaceAll("sex", "6") // funny suggested by drip
-        msg[0] = parseAnyNumber(msg[0])
-        if (/\D/.test(msg[0])) return
-        let {current_number: lastnumber, last_counter: lastcounter} = await queryone(db, "SELECT * FROM serverconfig WHERE server_id=?", [message.guild.id])
-        if (!lastnumber) {
-            lastnumber = 0
-        }
+        try {
+            let msg = message.content.toString().toLowerCase().split(" ")
+            msg[0] = msg[0].replaceAll("sex", "6") // funny suggested by drip
+            msg[0] = parseAnyNumber(msg[0])
+            if (/\D/.test(msg[0])) return
+            let {
+                current_number: lastnumber,
+                last_counter: lastcounter
+            } = await queryone(db, "SELECT * FROM serverconfig WHERE server_id=?", [message.guild.id])
+            if (!lastnumber) {
+                lastnumber = 0
+            }
 
-        if (lastcounter === message.member.id) {
-            return await message.reply({
-                embeds: [presets.warning("", "You cannot count twice in a row")]
-            }).then(newmessage => {
-                setTimeout(async () => {
-                    await newmessage.delete()
-                    await message.delete()
-                }, 3000)
-            })
-        }
-
-
-        if (Number(msg[0]) === Number(lastnumber) + 1) {
-            await execute(db, "UPDATE serverconfig SET current_number=?, last_counter=?, current_number_message_id=?  WHERE server_id=?", [Number(lastnumber) + 1, message.member.id, message.id, message.guild.id])
-            return await message.react("✅")
-        } else {
-            if (Number(lastnumber) === 0) {
-                await message.react("❌")
+            if (lastcounter === message.member.id) {
                 return await message.reply({
-                    embeds: [presets.warning("", "the current number is 1, please start over again")]
+                    embeds: [presets.warning("", "You cannot count twice in a row")]
                 }).then(newmessage => {
                     setTimeout(async () => {
                         await newmessage.delete()
+                        await message.delete()
                     }, 3000)
                 })
+            }
+
+
+            if (Number(msg[0]) === Number(lastnumber) + 1) {
+                await execute(db, "UPDATE serverconfig SET current_number=?, last_counter=?, current_number_message_id=?  WHERE server_id=?", [Number(lastnumber) + 1, message.member.id, message.id, message.guild.id])
+                return await message.react("✅")
+            } else {
+                if (Number(lastnumber) === 0) {
+                    await message.react("❌")
+                    return await message.reply({
+                        embeds: [presets.warning("", "the current number is 1, please start over again")]
+                    }).then(newmessage => {
+                        setTimeout(async () => {
+                            await newmessage.delete()
+                        }, 3000)
+                    })
+
+                }
+                await message.react("❌")
+                await message.reply({
+                    embeds: [presets.warning("FAIL", `<@${message.member.id}> messed up the count, the correct number was ${Number(lastnumber) + 1}`)]
+                })
+                await execute(db, "UPDATE serverconfig SET current_number=?, last_counter=?, altered_count=? WHERE server_id=?", [0, 0, 0, message.guild.id])
 
             }
-            await message.react("❌")
-            await message.reply({
-                embeds: [presets.warning("FAIL", `<@${message.member.id}> messed up the count, the correct number was ${Number(lastnumber) + 1}`)]
-            })
-            await execute(db, "UPDATE serverconfig SET current_number=?, last_counter=?, altered_count=? WHERE server_id=?", [0, 0, 0, message.guild.id])
 
+        } catch (e) {
+            console.error(e)
         }
-
-
-
     }
 }
